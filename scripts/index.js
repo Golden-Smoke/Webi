774e429ff0c4bd1293ae896658bcb22fd12a2778
Golden-Smoke <plamengeorgiev967@gmail.com>
2022-12-16 15:02:46 +0200

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,54 @@
+const faq_questions = document.querySelectorAll('.faq-question-cont')
+const grid_cards = document.querySelector('.recipes-grid')
+const button_next = document.getElementById('next')
+const button_prev = document.getElementById('prev')
+
+// Recipe Card - Gap
+let gap = 20
+//------------------
+faq_questions.forEach(faq_quest =>{
+    const faq_answer = faq_quest.nextElementSibling
+    faq_quest.addEventListener('click',() =>{
+        faq_quest.classList.toggle('open')
+
+        if(faq_quest.classList.contains('open')){
+            faq_answer.style.height = faq_answer.scrollHeight + 'px'
+        }
+        else{
+            faq_answer.style.height = 0
+        }
+    })
+})
+
+let card_width
+let countWidth = 0,counter = 1
+button_next.addEventListener('click',() =>{
+    if(counter == grid_cards.childElementCount - 1){
+        counter = 1
+        countWidth = 0
+        grid_cards.style.transform = `translate(0px)` 
+        return
+    }
+    counter++
+    card_width = grid_cards.firstElementChild.clientWidth + gap
+    countWidth += card_width  
+    grid_cards.style.transform = `translate(-${countWidth}px)` 
+})
+button_prev.addEventListener('click',() =>{
+    
+    if(counter == 1){
+        counter = grid_cards.childElementCount - 1
+        card_width = grid_cards.firstElementChild.clientWidth + gap
+        countWidth = card_width * (counter - 1)
+        grid_cards.style.transform = `translate(-${countWidth}px)`
+        return
+    }
+    counter--
+    card_width = grid_cards.firstElementChild.clientWidth + gap
+    countWidth -= card_width
+    grid_cards.style.transform = `translate(-${countWidth}px)` 
+    
+})
+ 
+
+ 
---
+++ scripts/layout.js
@@ -0,0 +1,57 @@
+const nav = document.querySelector('.nav-wrapper')
+const nav_links = document.querySelectorAll('.nav-link')
+const ham_cont = document.querySelector('.ham-wrapper > svg')
+const hamburger = document.querySelector('.ham-menu')
+const ham_close = document.querySelector('.ham-menu svg')
+const login_btn = document.querySelector('.nav-link.reg.space')
+const login_box = document.querySelector('.login')
+const login_close = document.querySelector('.login button')
+
+nav_links.forEach(link =>{
+    const copiedLink = link.cloneNode(true)
+    copiedLink.classList.remove('nav-link')
+    copiedLink.classList.add('ham-link')
+    hamburger.appendChild(copiedLink)
+})
+
+const login_mobile_link = document.querySelector('.ham-link.reg.space')
+
+function Login_Link(param){
+    param.addEventListener('click',() =>{
+        login_box.classList.add('show')
+    })
+}
+
+Login_Link(login_btn)
+Login_Link(login_mobile_link)
+
+login_close.addEventListener('click',() =>{
+    login_box.classList.remove('show')
+    document.body.style.overflow = 'initial'
+    document.body.style.paddingInlineStart = 'initial'
+})
+
+ham_close.addEventListener('click',() =>{
+    hamburger.classList.remove('open')
+})
+ham_cont.addEventListener('click',() =>{
+    hamburger.classList.add('open')
+})
+
+let lastScroll = 0;
+
+window.addEventListener('scroll',() =>{
+    let currentScroll = window.pageYOffset
+
+    if(currentScroll > lastScroll){
+        nav.classList.add('scroll-down')
+        nav.classList.remove('scroll-up')
+        hamburger.classList.remove('open')
+    }
+    if(currentScroll < lastScroll){
+        nav.classList.add('scroll-up')
+        nav.classList.remove('scroll-down')
+    }
+    lastScroll = currentScroll
+
+})
