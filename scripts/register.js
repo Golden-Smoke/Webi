700dd5adffe881967d09cd34480fa6ee0376914a
Golden-Smoke <plamengeorgiev967@gmail.com>
2022-12-18 19:58:50 +0200

    Updating Register Page and adding video

--- scripts/register.js
+++ scripts/register.js
@@ -2,12 +2,18 @@ const fields = document.querySelectorAll('.form-field')
 
 fields.forEach(field =>{
     const placeholder = field.querySelector('.placeholder')
-    const input_value = field.querySelector('.input-field').value
+   
     field.addEventListener('focusin',() =>{
         placeholder.classList.add('focused')
     })
 
     field.addEventListener('focusout',() =>{
-        if(input_value)  placeholder.classList.remove('focused')
+        let input_value = field.querySelector('.input-field').value
+        if(input_value.trim() == "")
+        {
+            placeholder.classList.remove('focused')
+            field.querySelector('.input-field').value = ''
+        }
+        else return
     })
 })
