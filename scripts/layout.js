f6fb19c0509e625a6f8bf8a7c0ff3b5b1ef910dd
Golden-Smoke <plamengeorgiev967@gmail.com>
2023-01-02 21:47:10 +0200

    Adding Login Page

--- scripts/layout.js
+++ scripts/layout.js
@@ -4,31 +4,12 @@ const ham_cont = document.querySelector(".ham-wrapper > svg");
 const hamburger = document.querySelector(".ham-menu");
 const ham_close = document.querySelector(".ham-menu svg");
 const login_btn = document.querySelector(".nav-link.reg.space");
-const login_box = document.querySelector(".login");
-const login_close = document.querySelector(".login button");
-
+ 
 nav_links.forEach((link) => {
   const copiedLink = link.cloneNode(true);
   copiedLink.classList.remove("nav-link");
   copiedLink.classList.add("ham-link");
   hamburger.appendChild(copiedLink);
-});
-
-const login_mobile_link = document.querySelector(".ham-link.reg.space");
-
-function Login_Link(param) {
-  param.addEventListener("click", () => {
-    login_box.classList.add("show");
-  });
-}
-
-Login_Link(login_btn);
-Login_Link(login_mobile_link);
-
-login_close.addEventListener("click", () => {
-  login_box.classList.remove("show");
-  document.body.style.overflow = "initial";
-  document.body.style.paddingInlineStart = "initial";
 });
 
 ham_close.addEventListener("click", () => {
